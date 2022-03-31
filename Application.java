53cc2c48aabbf6e0dde5593a4bc626d998ce3ae7
Vovanviet <vietvvth2107012@fpt.edu.vn>
2022-03-31 16:45:10 +0700

    Create Application.java

---
+++ Application.java
@@ -0,0 +1,12 @@
+package com.phonebook;
+
+import java.util.Scanner;
+
+public class Application {
+    public static void main(String[] args) {
+       Controller controller=new Controller();
+       controller.menu();
+
+
+    }
+}
