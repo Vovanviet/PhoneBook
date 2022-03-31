dbd3d8c2b18ccfcba57a20511dcbc44c768c875d
Vovanviet <vietvvth2107012@fpt.edu.vn>
2022-03-31 16:45:13 +0700

    Create Menu.java

---
+++ Menu.java
@@ -0,0 +1,11 @@
+package com.phonebook;
+
+public class Menu {
+    public static void mainMenu(){
+        System.out.println("------------------------------------");
+        System.out.println("\n1.Insert\n2.Delete\n3.LookUp\n4.Exit");
+    }
+    public static void accessDelete(){
+        System.out.println("\nAre you sure\n1.Yes\n2.No");
+    }
+}
